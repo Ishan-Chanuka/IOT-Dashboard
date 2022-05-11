a427dfda4c6cfeeae550d879cf8fba664d6099a2
Ishan-Chanuka <ishanchanuka4@gmail.com>
2022-05-11 11:25:20 +0530

    init commit

---
+++ IOTply/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using IOTply.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOTply.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Statics()
+        {
+            return View();
+        }
+        
+        public IActionResult IndiStatics()
+        {
+            return View();
+        }
+
+        public IActionResult ProfileMng()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
